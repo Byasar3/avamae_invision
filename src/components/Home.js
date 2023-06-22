1a65382d0f87c46484059168e009851c55f3bcd0
Berna <bernayasar@hotmail.co.uk>
2023-06-22 15:29:47 +0100

    added todo notes on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,24 @@ const Home = () => {
     return (
         <div>
             <CarouselContainer/>
-           
+            <div>
+                <div>
+                    part 2:
+                    image of office on RHS
+                    text on LHS with non functioning 'learn more' button
+                </div>
+                <div>
+                    part 3:
+                    image filling up full block
+                    darker grey box on image with text
+                    non functioning 'log in' button
+                </div>
+                <div>
+                    part 4:
+                    white background with column text
+                    functioning contact us button
+                </div>
+            </div>
         </div>
     )
 }

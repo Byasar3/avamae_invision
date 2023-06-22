e740ba4cb50b4a4566297849c8cf2f1573dafee4
Berna <bernayasar@hotmail.co.uk>
2023-06-22 14:04:48 +0100

    getting carousel to work - done but img wont load

--- src/containers/CarouselContainer.js
+++ src/containers/CarouselContainer.js
@@ -1,32 +1,41 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 
 const CarouselContainer = () => {
     
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        const fetchImages = () => {
-            axios.get('https://interview-assessment.api.avamae.co.uk/api/v1/home/banner-details')
-            .then(response => {
-                setImages(response.data.Details)
-                
-            })
-        }
-        fetchImages()
-        console.log('Response:', response);
+        axios({
+            method: 'get',
+            url: 'https://interview-assessment.api.avamae.co.uk/api/v1/home/banner-details'
+        }).then(response => {
+            setImages(response.data.Details)
+        })
     }, [])
 
-    const CarouselImages = images.map((image) => {
-        return <CarouselComponent
-        key={image.id}
-        image={image}/>
-    })
+    console.log('Hello from the container:', images);
 
     return (
-        <div>
-            {CarouselImages}
-        </div>
+        <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        >
+            {images.map(image => (
+            <SwiperSlide key={image.id}>
+                <img className='CarouselImages' src={image.ImageURL} alt={image.Subtitle} />
+                <div>
+                    <h1>{image.Title}</h1>
+                    <h2>{image.Subtitle}</h2>
+                    <button>Contact Us</button>
+                </div>
+            </SwiperSlide>
+            ))}
+        </Swiper>
     )
 }
 
